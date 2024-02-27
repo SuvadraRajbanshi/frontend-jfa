03d8a40de1053da5cb47ced184dc5dcbf362d95a
Suvadra Rajbanshi <suvadrarazz@gmail.com>
2024-02-27 19:58:30 +0545

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar';
+import Search from './Components/SearchDiv/Search';
+import Jobs from './Components/JobDiv/Jobs';
+
+const App = () => {
+  return (
+    <div className='w-[85%] m-auto bg-white'>
+      <NavBar/>
+      <Search/>
+      <Jobs/>
+     
+      </div>
+  )
+}
+
+export default App;
